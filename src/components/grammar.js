d4d648b3b4827c2679f5ac5f05ebb2fab8aa5b3f
Vigil, Daniel <Daniel.Vigil@cengage.com>
2019-08-13 13:21:11 -0400

    updates to add table of grammars

--- src/components/grammar.js
+++ src/components/grammar.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import Header from './header';
 import Footer from './footer';
-import CSVReader from 'react-csv-reader';
 import CommitBox from './commitbox';
+import XLSX from 'xlsx';
+import GrammarTable from './grammar_table';
 
 const Grammar = (props) => {
-
-    const readGrammarFile = (data) => {
-
+    
+    const handleFiles = (e) => {
+        const files = e.target.files, f = files[0];
+        //const workbook = XLSX.readFile(f.name);
+        console.log(f);
+        const reader = new FileReader();
+        reader.onLoad = (e) => {
+            //const data = new Uint8Array(e.target.result);
+            //const workbook = XLSX.read(data, {type: 'array'});
+            //const workbook = XLSX.readFile(f);
+            //console.log(workbook);
+        };
+        reader.readAsArrayBuffer(f);
     }
     return (
         <>
@@ -15,11 +26,17 @@ const Grammar = (props) => {
             <div className="main">
                 <h2>{props.name}</h2>
                 <div className="container">
-                    <form>
+                    {/*<form>
                         <div className="form-group">
                             <CSVReader cssInputClass="" label="Select CSV with Resources" onFileLoaded={readGrammarFile} />
                         </div>
+                        <label>Select a File</label>
+                        <input type="file" id="input" accept=".xlsx, .xls" onChange={handleFiles}></input>
                     </form>
+                    <button className="btn btn-primary">
+                        Create
+                    </button>*/}
+                    <GrammarTable />
                 </div>
                 <div className="container">
                     <CommitBox />
