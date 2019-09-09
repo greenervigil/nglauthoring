b74a5c963be24ac432bb41b8d655d0067784e013
Vigil, Daniel <Daniel.Vigil@cengage.com>
2019-09-09 14:03:59 -0400

    disabling grammar for until v2 for now

--- src/components/grammar_modal.js
+++ src/components/grammar_modal.js
@@ -1,20 +1,19 @@
 import React from 'react';
-import { Modal, Button, Icon, Header } from 'semantic-ui-react';
+import { Modal, Button, Header } from 'semantic-ui-react';
 import GrammarForm from './grammar_form';
 
 const GrammarModal = () => (
-    <Modal trigger={<Button floated='right'>Add Line</Button>} centered={false}>
+    <Modal trigger={<Button disabled floated='right'>Add Line</Button>} centered={false} >
         <Modal.Header>Grammar Entry</Modal.Header>
         <Modal.Content>
             <Modal.Description>
                 <Header>Enter in the grammar information.</Header>
+                <p>All numbers need to be spelled out in the answer text and illegal characters consisting of ".", ",", "?", "!", "-", "--", ";", "&#60;p&#62;", "&#60;/p&#62;", "’" and will be removed when the grammar file is created.</p>
             </Modal.Description>
             <GrammarForm />
         </Modal.Content>
         <Modal.Actions>
-            <Button primary>
-                Insert <Icon name='right chevron' />
-            </Button>
+            <Button floated='right' content="Finished"/>
         </Modal.Actions>
     </Modal>
 );
