094fca4ec15d47a2361f32f77260e9d8c250dd14
Vigil, Daniel <Daniel.Vigil@cengage.com>
2019-09-11 10:33:56 -0400

    ELTPROD-7412 finish compiling grm file for dev

--- src/components/grammar_create.js
+++ src/components/grammar_create.js
@@ -4,29 +4,46 @@ import { Button } from 'semantic-ui-react'
 const GrammarCreate = (props) => {
 
     const buildGrammar = (grammarArray) => {
+        //public <.GRAMMARID> = <ANSWER> | <ANSWER> | <ANSWER> | <ANSWER>;
+        //public <.GRAMMARID> = <ANSWER> | <ANSWER> | <ANSWER>;
+        //public <.GRAMMARID> = <ANSWER> | <ANSWER>;
+        //public <.GRAMMARID> = <ANSWER>;
         let grammarString = "public <." + grammarArray[0].substring(0, grammarArray[0].length-4) + grammarArray[1] + "> = "
-
-        if(grammarArray[3] !== '') {
-            grammarString += grammarArray[3].toLowerCase().replace(/!?,\./g, " ") + "| "
+        //answer 1
+        if (grammarArray[3] === '') {
+            //skip
+        }else if(grammarArray[3] !== '' || (grammarArray[3] === '' && grammarArray[4] !== '')) {
+            grammarString += grammarArray[3].toLowerCase().replace(/[!?,]/g, "").replace(/\./g, " ") + "| "
+        } else {
+            grammarString += grammarArray[3].toLowerCase().replace(/[!?,]/g, "").replace(/\./g, " ").trim() + "; "
         }
-        if(grammarArray[4] !== '') {
-            grammarString += grammarArray[4].toLowerCase().replace(/!\?,\./g, " ") + "| "
+        //answer 2
+        if (grammarArray[4] === '') {
+            //skip
+        } else if(grammarArray[4] !== '' && grammarArray[5] !== '') {
+            grammarString += grammarArray[4].toLowerCase().replace(/[!?,]/g, "").replace(/\./g, " ") + "| "
+        } else {
+            grammarString += grammarArray[4].toLowerCase().replace(/[!?,]/g, "").replace(/\./g, " ").trim() + "; "
         }
-        if(grammarArray[5] !== '') {
-            grammarString += grammarArray[5].toLowerCase().replace(/!\?,\./g, " ") + "| " 
-        } 
-        if(grammarArray[6] !== '') {
-            grammarString += grammarArray[6].toLowerCase().replace(/!\?,\./g, " ") + ";"
+        //answer 3
+        if (grammarArray[5] === ''){
+            //skip
+        } else if(grammarArray[5] !== '' && grammarArray[6] !== '') {
+            grammarString += grammarArray[5].toLowerCase().replace(/[!?,]/g, "").replace(/\./g, " ") + "| " 
+        }  else {
+            grammarString += grammarArray[5].toLowerCase().replace(/[!?,]/g, "").replace(/\./g, " ").trim() + "; "
+        }
+        //answer 4
+        if (grammarArray[6] === '') {
+            //skip
+        } else if(grammarArray[6] !== '') {
+            grammarString += grammarArray[6].toLowerCase().replace(/[!?,]/g, "").replace(/\./g, " ").trim() + "; "
         }        
 
         return grammarString
     }
 
     const handleClick = () => {
-        //public <.GRAMMARID> = <ANSWER> | <ANSWER> | <ANSWER> | <ANSWER>;
-        //public <.GRAMMARID> = <ANSWER> | <ANSWER> | <ANSWER>;
-        //public <.GRAMMARID> = <ANSWER> | <ANSWER>;
-        //public <.GRAMMARID> = <ANSWER>;
         let fileData = ''
         props.data
             .filter((row, index) => index !== 0)
