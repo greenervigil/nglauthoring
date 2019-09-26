0889ad3d49f6136f548ee6b4c1a0ab98cdaee362
Vigil, Daniel <Daniel.Vigil@cengage.com>
2019-09-26 15:19:27 -0400

    adding in engine specific processing for grm creation

--- src/components/grammar_create.js
+++ src/components/grammar_create.js
@@ -4,41 +4,45 @@ import { Button } from 'semantic-ui-react'
 const GrammarCreate = (props) => {
 
     const buildGrammar = (grammarArray) => {
-        //public <.GRAMMARID> = <ANSWER> | <ANSWER> | <ANSWER> | <ANSWER>;
+        //grammarId
+        let grammarString = "public <." + grammarArray[0].substring(0, grammarArray[0].length-4) + grammarArray[1] + "> = "
         //public <.GRAMMARID> = <ANSWER> | <ANSWER> | <ANSWER>;
         //public <.GRAMMARID> = <ANSWER> | <ANSWER>;
         //public <.GRAMMARID> = <ANSWER>;
-        let grammarString = "public <." + grammarArray[0].substring(0, grammarArray[0].length-4) + grammarArray[1] + "> = "
-        //answer 1
-        if (grammarArray[3] === '') {
-            //skip
-        }else if(grammarArray[3] !== '' || (grammarArray[3] === '' && grammarArray[4] !== '')) {
-            grammarString += grammarArray[3].toLowerCase().replace(/[!?,]/g, "").replace(/\./g, " ") + "| "
+        if (props.engine === 'Listening and Speaking') {
+            //answer 1
+            console.log(grammarArray[2])
+            if (grammarArray[2] === '') {
+                //skip
+            } else if(grammarArray[2] !== '' && grammarArray[3] !== '') {
+                grammarString += grammarArray[2].toLowerCase().replace(/[!?:,]/g, "").replace(/\./g, " ") + "| "
+            } else {
+                grammarString += grammarArray[2].toLowerCase().replace(/[!?:,]/g, "").replace(/\./g, " ").trim() + "; "
+            }
         } else {
-            grammarString += grammarArray[3].toLowerCase().replace(/[!?,]/g, "").replace(/\./g, " ").trim() + "; "
-        }
-        //answer 2
-        if (grammarArray[4] === '') {
-            //skip
-        } else if(grammarArray[4] !== '' && grammarArray[5] !== '') {
-            grammarString += grammarArray[4].toLowerCase().replace(/[!?,]/g, "").replace(/\./g, " ") + "| "
-        } else {
-            grammarString += grammarArray[4].toLowerCase().replace(/[!?,]/g, "").replace(/\./g, " ").trim() + "; "
-        }
-        //answer 3
-        if (grammarArray[5] === ''){
-            //skip
-        } else if(grammarArray[5] !== '' && grammarArray[6] !== '') {
-            grammarString += grammarArray[5].toLowerCase().replace(/[!?,]/g, "").replace(/\./g, " ") + "| " 
-        }  else {
-            grammarString += grammarArray[5].toLowerCase().replace(/[!?,]/g, "").replace(/\./g, " ").trim() + "; "
-        }
-        //answer 4
-        if (grammarArray[6] === '') {
-            //skip
-        } else if(grammarArray[6] !== '') {
-            grammarString += grammarArray[6].toLowerCase().replace(/[!?,]/g, "").replace(/\./g, " ").trim() + "; "
-        }        
+            //answer 1
+            if (grammarArray[4] === '') {
+                //skip
+            } else if(grammarArray[4] !== '' && grammarArray[5] !== '') {
+                grammarString += grammarArray[4].toLowerCase().replace(/[!?:,]/g, "").replace(/\./g, " ") + "| "
+            } else {
+                grammarString += grammarArray[4].toLowerCase().replace(/[!?:,]/g, "").replace(/\./g, " ").trim() + "; "
+            }
+            //answer 2
+            if (grammarArray[5] === ''){
+                //skip
+            } else if(grammarArray[5] !== '' && grammarArray[6] !== '') {
+                grammarString += grammarArray[5].toLowerCase().replace(/[!?:,]/g, "").replace(/\./g, " ") + "| " 
+            }  else {
+                grammarString += grammarArray[5].toLowerCase().replace(/[!?:,]/g, "").replace(/\./g, " ").trim() + "; "
+            }
+            //answer 3
+            if (grammarArray[6] === '') {
+                //skip
+            } else if(grammarArray[6] !== '') {
+                grammarString += grammarArray[6].toLowerCase().replace(/[!?:,]/g, "").replace(/\./g, " ").trim() + "; "
+            } 
+        }       
 
         return grammarString
     }
