efc9a95013ddbeb52d8621588f66cd9492bb982c
Vigil, Daniel <Daniel.Vigil@cengage.com>
2020-05-19 10:00:21 -0400

    update to footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,10 +2,15 @@ import React from 'react';
 import logo from '../default-xs.png'//'../ng_learning_whiteyellow.png';
 
 export default function Footer() {
+  const year = new Date().getUTCFullYear()
   return (
     <footer className="App-footer">
-      <img src={logo} alt="myelt logo"/>&nbsp;
-      &nbsp;&copy;&nbsp;{new Date().getUTCFullYear()} Cengage Learning
+      <div>
+      <img src={logo} alt="myelt logo"/>
+      </div>
+      <div>
+        <p data-testid="copy">&copy; {year} Cengage Learning</p>
+      </div>
     </footer>
   )
 }
