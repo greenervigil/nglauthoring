1171258d271adfa7d52326cfed9682dfad0fe14d
Vigil, Daniel <Daniel.Vigil@cengage.com>
2021-01-26 14:11:04 -0500

    refactor for react-magma update and navigation

--- src/pages/grammar.js
+++ src/pages/grammar.js
@@ -1,18 +1,12 @@
 import React from 'react'
-import Header from '../components/header'
-import Footer from '../components/footer'
 import { Alert } from 'react-magma-dom'
-import PropTypes from 'prop-types'
-import ReturnHome from '../components/main/return_home'
 import GrammarForm from '../components/grammar/grammar_form'
 
-export default function Grammar({ name }) {
+export default function Grammar() {
     return (
         <>
-        <Header />
-        <ReturnHome />
         <div className="container">
-            <h2>{name}</h2>
+            <h2>SRI Grammar</h2>
             <p>Upload CSV grammar file to evaluate grammar correctness, create SRI grammar files, and test bench configuration files.  <strong>All numbers need to be spelled out in the answer text to guarantee correct grammar is used.</strong></p>
             <div id="alert_error" role="alert" hidden>
                 <Alert dismissable="true" closeLabel="Close" variant="danger" onDismiss={() => document.getElementById('alert_error').setAttribute('hidden', true)}>Compile completed with errors.  Review the table and correct any errors.</Alert>
@@ -26,11 +20,6 @@ export default function Grammar({ name }) {
             <div id="grammarTable"></div>
         </div>
         &nbsp;
-        <Footer />
         </>
     )
-}
-
-Grammar.propTypes = {
-    name: PropTypes.string.isRequired
 }
